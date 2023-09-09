651d8bf41a91189b8125bd9454c7ebdfa5265fd2
Rafael93 <rafaelgamas@gmail.com>
2023-09-09 15:23:54 -0400

    bugs fixed and responsives features

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const weightInput = document.getElementById("weight-input");
 const calculate = document.getElementById("calculate-button");
-const container = document.getElementById("calculator");
+const resultContainer = document.getElementById("resultContainer");
 
 //El algoritmo debe funcionar de tal manera que al darle click
 //en el boton de calcular los resultados son mostrardos en la pantalla
 //El disparador se ejecutara a hacer click y llama a las funciones
 //que calculan la HB segun el peso del paciente
+
 calculate.addEventListener("click", function (event) {
-  event.preventDefault(); //se previene el comportamiento default de bootstrap
+  event.preventDefault(); //se previene el comportamiento default
+  hideResult();
   const weight = weightInput.value;
   let dailyVolume = 0;
   //condicion que comprueba el peso del paciente
@@ -18,88 +20,19 @@ calculate.addEventListener("click", function (event) {
     maintenancePlusHalf1 = mPlusHalfm(maintenanceVolume1);
     maintenancePlusHalf2 = mPlusHalfm(maintenanceVolume2);
 
-    //creamos dos elementos en el dom para insertar el volumen diario
-    const dailyVolumenResult1 = document.createElement("div");
-    const dailyVolumenResult2 = document.createElement("div");
-    //Agregando esta clase al div vamos a poder formatear su estilo
-    dailyVolumenResult1.classList.add("results");
-    dailyVolumenResult2.classList.add("results");
-    //insertar el texto en el html
-    dailyVolumenResult1.innerHTML =
-      "El volumen diario con constante 1500 debe ser " +
-      dailyVolume[0].toFixed(2) +
-      " cc";
-
-    container.appendChild(dailyVolumenResult1);
-
-    dailyVolumenResult2.innerHTML =
-      "El volumen diario con constante 2000 debe ser " +
-      dailyVolume[1].toFixed(2) +
-      " cc";
-
-    container.appendChild(dailyVolumenResult2);
-
-    //creamos elementos para mostrar el mantenimiento
-    const maintenanceResult1 = document.createElement("div");
-    maintenanceResult1.classList.add("results");
-
-    maintenanceResult1.innerHTML =
-      "El mantenimiento con constante 1500, en cc es " +
-      maintenanceVolume1.toFixed(2) +
-      " cc";
-    container.appendChild(maintenanceResult1);
-
-    const maintenanceResult2 = document.createElement("div");
-    maintenanceResult2.classList.add("results");
-
-    maintenanceResult2.innerHTML =
-      "El mantenimiento con constante 2000, en cc es " +
-      maintenanceVolume2.toFixed(2) +
-      " cc";
-    container.appendChild(maintenanceResult2);
-
-    //creamos elementos para mostrar el resultado del mantenimiento mas medio mantenimiento
-    const mplushmResult1 = document.createElement("div");
-    mplushmResult1.classList.add("results");
-
-    mplushmResult1.innerHTML =
-      "El mantenimiento mas medio mantenimiento con constante 1500, en cc es " +
-      maintenancePlusHalf1.toFixed(2) +
-      " cc";
-    container.appendChild(mplushmResult1);
-
-    const mplushmResult2 = document.createElement("div");
-    mplushmResult2.classList.add("results");
-
-    mplushmResult2.innerHTML =
-      "El mantenimiento mas medio mantenimiento con constante 2000, en cc es " +
-      maintenancePlusHalf2.toFixed(2) +
-      " cc";
-    container.appendChild(mplushmResult2);
+    printResultsBSM(
+      dailyVolume,
+      maintenanceVolume1,
+      maintenanceVolume2,
+      maintenancePlusHalf1,
+      maintenancePlusHalf2
+    );
   } else if (weight > 0 && weight <= 30) {
     dailyVolume = hollidaySegarMethod(weight);
     maintenanceVolume = maintenance(dailyVolume);
     maintenancePlusHalf = mPlusHalfm(maintenanceVolume);
 
-    //creamos un elemento en el dom para insertar el volumen diario
-    const result = document.createElement("div");
-    result.classList.add("results");
-    result.innerHTML = "El volumen diario debe ser " + dailyVolume + " cc";
-    container.appendChild(result);
-
-    //creamos un elemento en el dom para insertar el mantenimiento
-    const maintenanceResult = document.createElement("div");
-    maintenanceResult.classList.add("results");
-    maintenanceResult.innerHTML =
-      "El mantenimiento debe ser " + maintenanceVolume.toFixed(2) + " cc";
-    container.appendChild(maintenanceResult);
-
-    //creamos un elemento en el dom para instar el m+m/2
-    const maintenancePlusHalfResult = document.createElement("div");
-    maintenancePlusHalfResult.classList.add("results");
-    maintenancePlusHalfResult.innerHTML =
-      "El mantenimiento debe ser " + maintenancePlusHalf + " cc";
-    container.appendChild(maintenancePlusHalfResult);
+    printResultsHSM(dailyVolume, maintenanceVolume, maintenancePlusHalf);
   } else {
     window.alert("El peso debe ser mayor que 0");
   }
@@ -126,33 +59,14 @@ function hollidaySegarMethod(weight) {
       ? (volumen = volumen + (dosis % 1) * multiple * cantCC.secondKg)
       : null;
   } else if (dosis > 2) {
-    console.log(cantCC.firstKg);
     let tenKg = multiple * cantCC.firstKg;
     let twentyKg = multiple * cantCC.secondKg;
-    dosis > 1;
     volumen = tenKg + twentyKg;
     dosis > 1
       ? (volumen = volumen + (dosis % 1) * multiple * cantCC.lastKg)
       : null;
   }
 
-  /*
-	if (weight > 20) {
-    dosis = 10 * 100;
-    dosis = dosis + 10 * 50;
-    weight -= 20;
-    dosis = dosis + weight * 20;
-    return dosis;
-  } else if (weight > 10) {
-    dosis = 10 * 100;
-    weight -= 10;
-    dosis = dosis + weight * 50;
-    return dosis;
-  } else {
-    dosis = weight * 100;
-    return dosis;
-  }
-	*/
   return volumen;
 }
 
@@ -170,4 +84,125 @@ function bodySurfaceMethod(weight) {
   const multipleforFirstConst = bodySurface * 1500;
   const multipleforSecondConst = bodySurface * 2000;
   return [multipleforFirstConst, multipleforSecondConst];
+}
+
+function printResultsBSM(
+  dailyVolume,
+  maintenanceVolume1,
+  maintenanceVolume2,
+  maintenancePlusHalf1,
+  maintenancePlusHalf1
+) {
+  let container = document.querySelector(".container");
+  container.style.height = "90vh";
+  //creamos dos elementos en el dom para insertar el volumen diario
+  const dailyVolumenResult1 = document.createElement("div");
+  const dailyVolumenResult2 = document.createElement("div");
+  //Agregando esta clase al div vamos a poder formatear su estilo
+  dailyVolumenResult1.classList.add("results");
+  dailyVolumenResult2.classList.add("results");
+  //insertar el texto en el html
+  dailyVolumenResult1.innerHTML =
+    "El volumen diario con constante 1500 debe ser " +
+    dailyVolume[0].toFixed(2) +
+    "cc";
+
+  resultContainer.appendChild(dailyVolumenResult1);
+
+  dailyVolumenResult2.innerHTML =
+    "El volumen diario con constante 2000 debe ser " +
+    dailyVolume[1].toFixed(2) +
+    "cc";
+
+  resultContainer.appendChild(dailyVolumenResult2);
+
+  //creamos elementos para mostrar el mantenimiento
+  const maintenanceResult1 = document.createElement("div");
+  maintenanceResult1.classList.add("results");
+
+  maintenanceResult1.innerHTML =
+    "El mantenimiento con constante 1500, en cc es " +
+    maintenanceVolume1.toFixed(2) +
+    "cc";
+  resultContainer.appendChild(maintenanceResult1);
+
+  const maintenanceResult2 = document.createElement("div");
+  maintenanceResult2.classList.add("results");
+
+  maintenanceResult2.innerHTML =
+    "El mantenimiento con constante 2000, en cc es " +
+    maintenanceVolume2.toFixed(2) +
+    "cc";
+  resultContainer.appendChild(maintenanceResult2);
+
+  //creamos elementos para mostrar el resultado del mantenimiento mas medio mantenimiento
+  const mplushmResult1 = document.createElement("div");
+  mplushmResult1.classList.add("results");
+
+  mplushmResult1.innerHTML =
+    "El mantenimiento mas medio mantenimiento con constante 1500, en cc es " +
+    maintenancePlusHalf1.toFixed(2) +
+    "cc";
+  resultContainer.appendChild(mplushmResult1);
+
+  const mplushmResult2 = document.createElement("div");
+  mplushmResult2.classList.add("results");
+
+  mplushmResult2.innerHTML =
+    "El mantenimiento mas medio mantenimiento con constante 2000, en cc es " +
+    maintenancePlusHalf2.toFixed(2) +
+    "cc";
+  resultContainer.appendChild(mplushmResult2);
+}
+
+function printResultsHSM(dailyVolume, maintenanceVolume, maintenancePlusHalf) {
+  const container = document.querySelector(".container");
+  container.style.height = "80vh";
+
+  const dailyVolumenContainer = document.createElement("div");
+  dailyVolumenContainer.classList.add("results");
+
+  const dailyVolumeResult = document.createElement("span");
+  dailyVolumeResult.classList.add("results");
+
+  dailyVolumenContainer.innerHTML = "El volumen diario debe ser ";
+  dailyVolumeResult.innerHTML = dailyVolume + "cc";
+
+  resultContainer.appendChild(dailyVolumenContainer);
+  resultContainer.appendChild(dailyVolumeResult);
+
+  //creamos un elemento en el dom para insertar el mantenimiento
+  const maintenanceContainer = document.createElement("div");
+  maintenanceContainer.classList.add("results");
+
+  const maintenanceResult = document.createElement("span");
+  maintenanceResult.classList.add("results");
+
+  maintenanceContainer.innerHTML = "El mantenimiento debe ser ";
+  maintenanceResult.innerHTML = maintenanceVolume.toFixed(2) + "cc";
+
+  resultContainer.appendChild(maintenanceContainer);
+  resultContainer.appendChild(maintenanceResult);
+
+  //creamos un elemento en el dom para instar el m+m/2
+  const maintenancePlusHalfContainer = document.createElement("div");
+  maintenancePlusHalfContainer.classList.add("results");
+
+  const maintenancePlusHalfResult = document.createElement("span");
+  maintenancePlusHalfResult.classList.add("results");
+
+  maintenancePlusHalfContainer.innerHTML = "El mantenimiento debe ser ";
+  maintenancePlusHalfResult.innerHTML = maintenancePlusHalf.toFixed(2) + "cc";
+
+  resultContainer.appendChild(maintenancePlusHalfContainer);
+  resultContainer.appendChild(maintenancePlusHalfResult);
+}
+
+function hideResult() {
+  let results = document.querySelectorAll(".results");
+  if (results.length > 0) {
+    for (let i = 0; i < results.length; i++) {
+      results[i].remove();
+    }
+  }
 }
